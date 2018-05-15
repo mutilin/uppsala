c8911f2c513b7a53925714b5e16ecc3865207c93
Vadim Mutilin <mutilin@ispras.ru>
2018-05-15 16:40:23 +0300

    Fix asserts

--- adhoc/init_var_happens_before_ptr_true-unreach-race.c
+++ adhoc/init_var_happens_before_ptr_true-unreach-race.c
@@ -5,7 +5,7 @@ void __VERIFIER_assume(int);
 int *ptr = 0;
 int init_var = 0;
 
-int *get_ptr(void) {
+void get_ptr(int i) {
         pthread_mutex_lock(&mutex);
         if(ptr==0) {
 		if(!init_var) {
@@ -14,32 +14,28 @@ int *get_ptr(void) {
 		        int *tmp;
 		        tmp = (int *)malloc(sizeof(int));
 	                __VERIFIER_assume(tmp!=0);
-			*tmp = 1;
+			*tmp = i;
 		        pthread_mutex_lock(&mutex);
 			ptr = tmp;
 		        pthread_mutex_unlock(&mutex);
+			//init_var && ptr!=0 makes a happens before edge 
+			//preventing read before initialization
+			ldv_assert(*ptr == i);
 		} else {
 			pthread_mutex_unlock(&mutex);
 		}
         } else {
 	        pthread_mutex_unlock(&mutex);
         }
-	return 0;
 }
 
 void *t1(void* arg) {
-        int *p = get_ptr();
-        //init_var && ptr!=0 makes a happens before edge 
-        //preventing read before initialization
-        ldv_assert(*p == 1);
+        get_ptr(1);
 	return 0;
 }
 
 void *t2(void* arg) {
-        int *p = get_ptr();
-        //init_var && ptr!=0 makes a happens before edge 
-        //preventing read before initialization
-        ldv_assert(*p == 1);
+        get_ptr(2);
 	return 0;
 }
 
