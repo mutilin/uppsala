91fe351b0d61e43a2140751010f8b361b1436904
verudenchik <verudenchik@gmail.com>
2022-04-18 15:12:07 +0300

    Add test

---
+++ adhoc/init-synchronization_false-unreach-call.c
@@ -0,0 +1,34 @@
+#include <pthread.h>
+
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+int __VERIFIER_nondet_int(void);
+void ldv_assert(int expression) { if (!expression) { ERROR: __VERIFIER_error();}; return; }
+
+pthread_t t1;
+pthread_mutex_t mutex;
+int pdev;
+int init;
+
+void *thread1(void *arg) {
+   pthread_mutex_lock(&mutex);
+   if (init ==0) {
+       return;
+   }
+   pthread_mutex_unlock(&mutex);
+   pdev = 6;
+   ldv_assert(pdev==6);
+}
+
+void *thread2(void *arg) {
+   if (init == 0) {
+        init = 1;
+        pdev = 1;
+   }
+}
+
+int main(void) {
+    pthread_t t1, t2;
+    pthread_create(&t1, NULL, thread1, NULL);
+    pthread_create(&t2, NULL, thread2, NULL);
+    return 0;
+}
